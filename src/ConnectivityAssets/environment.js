c31662004f64476cea8e2deba3ab6cd89a439686
UsmanShabbir295 <usmanshabbir295@gmail.com>
2025-01-06 22:52:21 +0500

    Updates

--- src/ConnectivityAssets/environment.js
+++ src/ConnectivityAssets/environment.js
@@ -1,3 +1,3 @@
 export const tokenAddress = "0x718fE1Cb67275e9eC357fbe5793905bb4d8C1409";
 export const usdtAddress = "0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814";
-export const presaleAddress = "0xDD5A46Cc71828Fc989Fd01537EdDE896ac368c9C";
+export const presaleAddress = "0x58E8b9ee4aA77A126b0425F3aA031C56f19603ae";
