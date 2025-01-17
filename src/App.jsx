0056737bba5dbb7b08b934d2847746331a12c59d
awais994 <awaisjaved994@gmail.com>
2025-01-17 19:17:01 +0500

    update loading

--- src/App.jsx
+++ src/App.jsx
@@ -14,38 +14,54 @@ import {
 } from "./pages";
 import { AppContext } from "./utils/utils";
 import { Routes, Route } from "react-router-dom";
+import Loading from "./components/SmallComponents/loading";
 
 function App() {
   const { account, chainId } = useContext(AppContext);
   const [openNetworkSwitch, setOpenNetworkSwitch] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (account && chainId > 0 && chainId !== 56) {
       setOpenNetworkSwitch(true);
     }
   }, [chainId, account]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <NetworkSwitch open={openNetworkSwitch} setOpen={setOpenNetworkSwitch} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HeroSection />
-              <AddressSection />
-              <Presale />
-              <Bloodline />
-              <Dragonomics />
-              <HowToBuy />
-              {/* <Partners /> */}
-              <TargaryenRoadmap />
-              {/* <Faq /> */}
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
+      {loading ? (
+        <Loading loading={loading} /> // Display loader while loading
+      ) : (
+        <>
+          <NetworkSwitch
+            open={openNetworkSwitch}
+            setOpen={setOpenNetworkSwitch}
+          />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <HeroSection />
+                  <AddressSection />
+                  <Presale />
+                  <Bloodline />
+                  <Dragonomics />
+                  <HowToBuy />
+                  {/* <Partners /> */}
+                  <TargaryenRoadmap />
+                  {/* <Faq /> */}
+                  <Footer />
+                </>
+              }
+            />
+          </Routes>
+        </>
+      )}
     </>
   );
 }
--- src/components/SmallComponents/loading.jsx
+++ src/components/SmallComponents/loading.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
+import { Box } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
 import { RingLoader } from "react-spinners";
+import gifLoader from "../../assets/dragonMainImg.gif";
 
 export default function Loading({ loading }) {
   return (
@@ -9,10 +11,19 @@ export default function Loading({ loading }) {
         sx={{
           color: "#fff",
           zIndex: 220000000,
+          backgroundColor: "rgba(0, 0, 0, 0.9)",
         }}
         open={loading}
       >
-        <RingLoader color="#ffffff" size={150} />
+        {/* <RingLoader color="#ffffff" size={150} /> */}
+        <Box
+          component={"img"}
+          alt=""
+          src={gifLoader}
+          sx={{
+            width: { xs: "200px", md: "350px" },
+          }}
+        />
       </Backdrop>
     </div>
   );
