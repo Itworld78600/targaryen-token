8973ede349ab9a42051f1747abe3fc04d412cb79
awais994 <awaisjaved994@gmail.com>
2025-01-17 19:26:56 +0500

    add wert model

--- src/components/SmallComponents/loading.jsx
+++ src/components/SmallComponents/loading.jsx
@@ -1,29 +1,37 @@
 /* eslint-disable react/prop-types */
 import { Box } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
-import { RingLoader } from "react-spinners";
-import gifLoader from "../../assets/dragonMainImg.gif";
+import { logo2 } from "./Images";
 
 export default function Loading({ loading }) {
   return (
     <div>
       <Backdrop
         sx={{
-          color: "#fff",
           zIndex: 220000000,
-          backgroundColor: "rgba(0, 0, 0, 0.9)",
         }}
         open={loading}
       >
-        {/* <RingLoader color="#ffffff" size={150} /> */}
         <Box
-          component={"img"}
-          alt=""
-          src={gifLoader}
+          width="100%"
+          height="100%"
           sx={{
-            width: { xs: "200px", md: "350px" },
+            background:
+              "linear-gradient(rgba(5, 4, 1, 0.6), rgba(5, 4, 1, 0.7))",
+            backdropFilter: "blur(20px)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        />
+        >
+          <Box
+            className="fadeinout"
+            component={"img"}
+            src={logo2}
+            width={{ xs: "180px", md: "350px" }}
+            alt=""
+          />
+        </Box>
       </Backdrop>
     </div>
   );
